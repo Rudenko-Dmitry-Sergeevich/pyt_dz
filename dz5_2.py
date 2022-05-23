55477fd219c68e8eb962a49742e6c9dfcc2bc2d4
Dima <dima031184@mail.ru>
2022-05-23 18:33:54 +0300

    Домашнее задание к lesson-5

---
+++ dz5_2.py
@@ -0,0 +1,9 @@
+# 2. Создать текстовый файл (не программно), сохранить в нём несколько строк, выполнить подсчёт строк и слов в каждой строке.
+with open('new_file.txt') as new_file:
+    y = 1
+    i = new_file.readlines()
+    print(i)
+    print(f'В файле {len(i)} строк')
+    for x in i:
+        print(f'{y}) str - {len(x.split())} слов')
+        y +=1
---
+++ dz5_3.py
@@ -0,0 +1,11 @@
+# 3. Создать текстовый файл (не программно).
+# Построчно записать фамилии сотрудников и величину их окладов (не менее 10 строк).
+# Определить, кто из сотрудников имеет оклад менее 20 тысяч, вывести фамилии этих сотрудников.
+# Выполнить подсчёт средней величины дохода сотрудников.
+with open('workers.txt') as worker_list:
+    worker_str = (i.split() for i in worker_list)
+    worker_dict = {a[0]: a[1] for a in worker_str}
+    print(f'перевел строки из файла в словарь: {worker_dict}')
+    worker_min = [i for i in worker_dict if float(worker_dict[i]) < 20000]
+    print(f'сотрудники с окладом менее 20 тысяч: {worker_min}')
+    print(f'средняя зарплата сотрудников: {round(sum([float(i) for i in worker_dict.values()]) / len(worker_dict), 2)}')
