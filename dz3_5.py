32148a36e840fa7e2e099a2964615fb8a653ea5f
Dima <dima031184@mail.ru>
2022-06-03 22:52:09 +0300

    Домашнее задание к lesson-3

--- dz3_5.py
+++ dz3_5.py
@@ -1,14 +1,17 @@
 # задание №5
-b = 0
-c = 0
-while True:
-    if c == 'n':
-        break
-    a = input('введите числа через пробел, чтобы остановить программу введите <n>')
-    a = a.split()
-    for i in range(0,len(a)):
-        if a[i] == 'n':
-            c = 'n'
+
+def my_func():
+    b = 0
+    c = 0
+    while True:
+        if c == 'n':
             break
-        b += int(a[i])
-    print(f'сумма чисел: {b}')
+        a = input('введите числа через пробел, чтобы остановить программу введите <n>')
+        a = a.split()
+        for i in range(0,len(a)):
+            if a[i] == 'n':
+                c = 'n'
+                break
+            b += int(a[i])
+        print(f'сумма чисел: {b}')
+my_func()
